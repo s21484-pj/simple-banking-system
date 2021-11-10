3528d3cdd6c85ebc20834ab020c7bab0c835c226
MaciejLeciejewski <56191731+s21484-pj@users.noreply.github.com>
2021-11-10 22:16:16 +0100

    luhn algorithm implementation

--- Simple Banking System/task/src/banking/Card.java
+++ Simple Banking System/task/src/banking/Card.java
@@ -1,5 +1,6 @@
 package banking;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Card {
@@ -40,15 +41,44 @@ public class Card {
         this.balance = balance;
     }
 
+    private int luhnAlgorithm(String id) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        int sum = 0;
+        for (int i = 0; i < id.length(); i++) {
+            digits.add(Integer.parseInt(String.valueOf(id.charAt(i))));
+            if (i % 2 == 0) {
+                int temp = digits.get(i) * 2;
+                digits.set(i, temp);
+            }
+            if (digits.get(i) > 9) {
+                digits.set(i, digits.get(i) - 9);
+            }
+        }
+        for (Integer digit : digits) {
+            sum += digit;
+        }
+        int temp = sum % 10;
+        int checksum = 0;
+        if (temp != 0) {
+            while (temp < 10) {
+                checksum++;
+                temp++;
+            }
+        }
+        return checksum;
+    }
+
     public void generateId() {
         final String BIN = "400000";
         Random random = new Random();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 9; i++) {
             int temp = random.nextInt(10);
             stringBuilder.append(temp);
         }
         String idAsString = BIN + stringBuilder;
+        int checksum = luhnAlgorithm(idAsString);
+        idAsString = idAsString.concat(String.valueOf(checksum));
         Long id = Long.parseLong(idAsString);
         this.setId(id);
     }
